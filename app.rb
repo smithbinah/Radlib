b6cde9cb4e660938e90c2708772697a995b2ba99
Unknown <brbodily@student.neumont.edu>
2018-04-23 18:41:27 -0600

    Upload post command

--- app.rb
+++ app.rb
@@ -15,6 +15,26 @@ get "/#{:story}" do
   erb(:story)
 end
 
+# post "/upload" do
+
+# 	file_name = params['story_file'][:filename]
+# 	file_contents = ""
+
+# 	if File.exists?(file_name)
+
+# 		File.open(file_name, 'r') do |file|
+# 			file_contents = file.read
+# 		end
+# 	end
+
+# 	if file_contents != ""
+
+# 		File.open("uploads/" + file_name, "w") do |file|
+# 			file.puts file_contents
+# 		end
+# 	end
+# end
+
 # finish up when upload.erb is finished
 post "/upload" do
   upload << params[:storyFile]
